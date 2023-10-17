743434dd6c9a78a553c5dc4e5c17c317c0075bc3
Clayton Bentes <claytonbentes@live.com>
2023-10-17 20:43:55 -0300

    Foreign key not null

---
+++ SalesWebMvc/Migrations/20231017233902_DepartmentForeignKey.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class DepartmentForeignKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seller_Department_DepaartmentId",
+                table: "Seller");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Seller_DepaartmentId",
+                table: "Seller");
+
+            migrationBuilder.DropColumn(
+                name: "DepaartmentId",
+                table: "Seller");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DepartmentId",
+                table: "Seller",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seller_DepartmentId",
+                table: "Seller",
+                column: "DepartmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seller_Department_DepartmentId",
+                table: "Seller",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seller_Department_DepartmentId",
+                table: "Seller");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Seller_DepartmentId",
+                table: "Seller");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentId",
+                table: "Seller");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DepaartmentId",
+                table: "Seller",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seller_DepaartmentId",
+                table: "Seller",
+                column: "DepaartmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seller_Department_DepaartmentId",
+                table: "Seller",
+                column: "DepaartmentId",
+                principalTable: "Department",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
